8d477e8622f5344145d145d09c9219e806242cb1
Romain Pouclet <romain.pouclet@gmail.com>
2017-09-21 08:48:42 +0200

    Remove debug code in UI Tests

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -4,26 +4,16 @@ class RomanNumeralCalculatorUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        continueAfterFailure = false
+        XCUIApplication().launch()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
     func testExample() {
-        // Use recording to get started writing UI tests.
-        
-        
         XCUIDevice.shared().orientation = .portrait
         
         let app = XCUIApplication()
@@ -32,25 +22,11 @@ class RomanNumeralCalculatorUITests: XCTestCase {
         app.buttons["+"].tap()
         iButton.tap()
 
-
-        //app.b
-        //XCTAssert(app.labels["calculated_value"].title == "II")
-
-        print(app.debugDescription)
-        print("value = " + (app.staticTexts["calculated_value"].value as? String)!)
-
         let label = app.staticTexts["calculated_value"]
         let exists = NSPredicate(format: "label == 'II'")
 
-
-
         expectation(for: exists, evaluatedWith: label, handler: nil)
         waitForExpectations(timeout: 30, handler: nil)
-
-        print(app.debugDescription)
-        print("label title = " + (label.value as! String));
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
 }
